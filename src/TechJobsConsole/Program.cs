e77869a7d98dd0fedfc278a04fd0051b08e1cae5
acnaumann <alysson12985@gmail.com>
2020-01-04 18:22:53 -0600

    implemented printJobs for "list all"

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -118,7 +118,17 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("printJobs is not implemented yet");
+            //Console.WriteLine("printJobs is not implemented yet");
+            foreach (Dictionary<string, string> dictionary in someJobs)
+            {
+                Console.WriteLine("*****");
+                foreach (KeyValuePair<string, string> kvp in dictionary)
+                {
+                    Console.WriteLine(kvp.Key + ": " + kvp.Value);
+                }
+                Console.WriteLine("*****");
+            }
+
         }
     }
 }
